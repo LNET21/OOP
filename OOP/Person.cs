13756e9a9f0df87b81cfbdb05bf51187d38e4f0d
Dimitris Björlingh <dimitris.bjorlingh@outlook.com>
2021-06-21 13:59:50 +0200

    Person, Employee, Admin

---
+++ OOP/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    class Person
+    {
+        public string Name { get; set; }
+    }
+
+    class Employee  : Person
+    {
+        public int Salary { get; set; }
+    }
+
+    class Admin : Employee
+    {
+        public string Department { get; set; }
+    }
+}
--- OOP/Program.cs
+++ OOP/Program.cs
@@ -6,7 +6,19 @@ namespace OOP
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Person pers = new Admin();
+            Person pers2 = new Employee();
+            Person pers3 = new Person();
+            Admin admin = new();
+            
+
+            pers.Name = "Nisse";
+            pers2.Name = "Anna";
+            Employee castad = (Employee)pers2;
+
+            admin.
+            
+            
         }
     }
 }
